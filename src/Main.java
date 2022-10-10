c5688eccf931d7fd2fc34d9de7bcce2a30267243
Mario Martinez <Mariostiveng@hotmail.com>
2022-10-10 07:54:33 -0500

    Incio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Cliente cliente = new Cliente();
+        cliente.nombre = "Jose";
+        cliente.edad = 32;
+        cliente.telefono = "3128652346";
+        cliente.credito = 1990;
+
+        System.out.println("Estos son los datos del Cliente");
+        System.out.println("Nombre " + cliente.nombre);
+        System.out.println("Edad " + cliente.edad);
+        System.out.println("Telefono " + cliente.telefono);
+        System.out.println("Credito " + cliente.credito);
+        System.out.println("");
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.nombre = "Carlos";
+        trabajador.edad = 36;
+        trabajador.telefono = "3148653283";
+        trabajador.saldo = 2000;
+
+        System.out.println("Estos son lso datos del Trabajador");
+        System.out.println("Nombre " + trabajador.nombre);
+        System.out.println("Edad " + trabajador.edad);
+        System.out.println("Telefono " + trabajador.telefono);
+        System.out.println("Saldo " + trabajador.saldo);
     }
+
+}
+class Persona{
+    String nombre;
+    int edad;
+    String telefono;
+
+}
+class Cliente extends Persona{
+    float credito;
+}
+
+class Trabajador extends  Persona{
+    float saldo;
 }
